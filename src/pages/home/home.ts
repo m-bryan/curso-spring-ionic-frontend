7615ad1651c18a507ebf24229c6c50b58609b7b1
Bryan <michaelbryan.eng@gmail.com>
2019-11-12 23:17:27 -0300

    Aproveitando usuario logado na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -35,6 +35,15 @@ export class HomePage {
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
   }
+
+  ionViewDidEnter() {
+    this.auth.refreshToken()
+      .subscribe(response => {
+        this.auth.successfulLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      error => {});
+  }
   
   //habilita menu na pagina de login
   ionViewDidLeave() {
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -35,6 +35,16 @@ export class AuthService {
         this.storage.setLocalUser(user);
     }
 
+    refreshToken() {
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/auth/refresh_token`, 
+            {},
+            {
+                observe: 'response',
+                responseType: 'text'
+            })
+    }
+
     logout() {
         this.storage.setLocalUser(null);
     }
